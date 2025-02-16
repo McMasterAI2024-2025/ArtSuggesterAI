e33048a0b0200706edf5f23b90593045cd0e0b13
Angela <angi.sf7@gmail.com>
2025-02-16 11:21:09 -0500

    star button

--- frontend/art-suggester-website/src/pages/Suggested.js
+++ frontend/art-suggester-website/src/pages/Suggested.js
@@ -20,26 +20,25 @@ export default function Suggested() {
     function delFav(toDelIndex){
         const newSuggested = suggested.filter((suggestedItem,index) => suggestedItem !== index);
         setSuggested (newSuggested);
-
     }
 
     return (
         <>
             <NavBar />
             <h2>Suggested Page</h2>
-            {/*<button onClick={() => {addFav()}}>Add Favourite</button>*/}
+            {/* <button onClick={() => {addFav()}}>Add Favourite</button> */}
             <div className="main">
             <ul className = 'favourites'>
             {suggested.map ((suggestedItem, index) => {
                 return (
                     <div className="favItem" key = {index}>
-                        {/*<li>{suggestedItem}</li>*/}
+                        {/* <li>{suggestedItem}</li> */}
                         <img src={`http://localhost:5000/suggestedImages/${index}`} alt="image" className="img"/>
-                        {/*
+                        {
                         <div className="icons">
                             <FontAwesomeIcon icon={faDownload} className="download"/>
-                            <FontAwesomeIcon icon={faStar} className="star" onClick={() => {delFav(index)}}/>
-                        </div>*/}
+                            <FontAwesomeIcon icon={faStar} className="star" onClick={() => delFav(index)}/>
+                        </div>}
                     </div>
                 )
             }
